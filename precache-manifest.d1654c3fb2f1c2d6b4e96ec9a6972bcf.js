84adb867c62f726388aad3ca2f36098877985f39
Kim <giminsub1@gmail.com>
2020-09-08 11:12:26 +1000

    Updates

---
+++ precache-manifest.d1654c3fb2f1c2d6b4e96ec9a6972bcf.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8783739609ba97a312eddca43566b237",
+    "url": "/small-business/index.html"
+  },
+  {
+    "revision": "7903c4edf92fc9096bfd",
+    "url": "/small-business/static/css/main.c7a304d5.chunk.css"
+  },
+  {
+    "revision": "e513d2ab1985e10f5b60",
+    "url": "/small-business/static/js/2.47943d23.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/small-business/static/js/2.47943d23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7903c4edf92fc9096bfd",
+    "url": "/small-business/static/js/main.73a413cd.chunk.js"
+  },
+  {
+    "revision": "1b715d6068d68db1790b",
+    "url": "/small-business/static/js/runtime-main.589090a9.js"
+  },
+  {
+    "revision": "e37ec8d51dd960a7678db936e98c134e",
+    "url": "/small-business/static/media/Duyoung.e37ec8d5.png"
+  },
+  {
+    "revision": "a6f83bb1033fa3a7d389c333517a4e9e",
+    "url": "/small-business/static/media/Insub.a6f83bb1.jpg"
+  },
+  {
+    "revision": "e476a1a593ede9a97d8fe7e6394eafe2",
+    "url": "/small-business/static/media/Sohwa.e476a1a5.jpg"
+  },
+  {
+    "revision": "8bae73f76936dc5220660c87d176d049",
+    "url": "/small-business/static/media/W_deep_head.8bae73f7.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/small-business/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5dfd25c92d5cda992f1317bf8c9d06b9",
+    "url": "/small-business/static/media/main_bg.5dfd25c9.jpg"
+  },
+  {
+    "revision": "f40ed5b1e791e9d2479ba8b5e67f2df8",
+    "url": "/small-business/static/media/map-image.f40ed5b1.png"
+  }
+]);
