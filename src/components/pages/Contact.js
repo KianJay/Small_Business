8952c9279fec1fed89f3cb297053abc9ca5dc14e
Kim <giminsub1@gmail.com>
2020-09-15 11:47:19 +1000

    Contact another google map added

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -8,6 +8,12 @@ const Contact = () => {
   return (
     <div className="Contact">
       <ContactUsMail />
+      
+      <iframe width="2030" height="630" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" id="gmap_canvas" 
+      src="https://maps.google.com/maps?width=630&amp;height=576&amp;hl=en&amp;q=160%20Sussex%20Sydney+()&amp;t=&amp;z=16&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">
+      </iframe> 
+      <script type='text/javascript' src='https://maps-generator.com/google-maps-authorization/script.js?id=3a19da6b02317fd004cf13ba1367d8c0e761e69e'>
+      </script>
       <Gmap />
       <br />
       <br />
